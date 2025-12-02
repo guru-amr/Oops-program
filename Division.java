6dc0b6c0fcd51bc7e8faf606d0a2541a43bef864
GURURAJASEKAR P <gururajasekar25@gmail.com>
2025-12-02 10:41:01 +0530

    summa

---
+++ Division.java
@@ -0,0 +1,36 @@
+package practice;
+class Racer extends Thread {
+ private String name;
+ private int sleepTime; // Time between each step
+ public Racer(String name, int sleepTime) {
+ this.name = name;
+ this.sleepTime = sleepTime;
+ }
+ // run() defines the race logic
+ public void run() {
+ for (int step = 1; step <= 5; step++) {
+ System.out.println(name + " ran step " + step);
+ try {
+	 Thread.sleep(null); // Sleep to simulate running time
+ } 
+ catch (InterruptedException e) {
+	 System.out.println(name + " was interrupted!");
+ }
+ }
+ System.out.println(name + " has finished the race!");
+ }
+}
+// Main class to simulate the race
+public class Division {
+ public static void main(String[] args) {
+ Racer racerA = new Racer("Thread A", 0);
+ Racer racerB = new Racer("Thread B", 0);
+ 
+ racerA.start();
+ racerB.start();
+ 
+ 
+ }
+}
+
+
